d9750d9cd0fcbc0feedab23cf81efeb18ecafd34
maninder2295 <65013546+maninder2295@users.noreply.github.com>
2022-03-26 14:55:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+import string
 
-l = alphabet*2
+alphabet = list(string.ascii_lowercase)*2
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
